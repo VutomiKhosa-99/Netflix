c6506112df20dcc7afae5167cf1737adfe5140f7
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-01-12 17:14:09 +0200

    users and auth  routes created successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.scss'
+import React from 'react'
 import Home from './pages/home/Home'
+import Register from './pages/register/Register'
 import Watch from './pages/watch/Watch'
+import './App.scss'
+import Login from './pages/login/Login'
 
 function App() {
 
   return (
+    <div>
       <Home />
+    </div>
   )
 }
 
