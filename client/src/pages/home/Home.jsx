6a1a916971bba6e26f67284a9b56c51cb32340bd
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-01-19 18:31:54 +0200

    Added jsonconfig file

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-// import Navbar from "../../components/navbar/Navbar";
-import Featured from "../../components/featured/Featured";
-import "./home.scss";
-import List from "../../components/list/List";
 import { useEffect, useState } from "react";
+import "./home.scss";
 import axios from "axios";
+import Navbar from "components/navbar/Navbar";
+import Featured from "components/featured/Featured";
+import List from "components/list/List";
 
 const Home = ({ type }) => {
   const [lists, setLists] = useState([]);
